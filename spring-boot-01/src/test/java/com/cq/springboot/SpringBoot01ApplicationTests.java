81ce5be6b0132268d3898f3c4dc11846eebf5400
彭国仁 <19112471511@qq.com>
2020-01-04 18:51:49 +0800

    springboot日志

--- spring-boot-01/src/test/java/com/cq/springboot/SpringBoot01ApplicationTests.java
+++ spring-boot-01/src/test/java/com/cq/springboot/SpringBoot01ApplicationTests.java
@@ -2,6 +2,8 @@ package com.cq.springboot;
 
 import com.cq.springboot.bean.Person;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -15,6 +17,15 @@ class SpringBoot01ApplicationTests {
     ApplicationContext ioc;
 
 
+    @Test
+    public void testLogging() {
+        Logger logger = LoggerFactory.getLogger(SpringBoot01ApplicationTests.class);
+        logger.trace("你好trace");
+        logger.debug("你好debug");
+        logger.info("你好info");
+        logger.warn("你好warn");
+        logger.error("你好error");
+    }
     @Test
     public void testHelloService() {
         boolean b = ioc.containsBean("helloservice");
